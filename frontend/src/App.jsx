1e6e0f7f738a74c61184b520a44d3f2fb7669323
Akshat Panchal <akshat.coder.04@gmail.com>
2025-08-26 09:00:56 +0530

    Login signup page frontend (#7)
    * Developed frontend of Login/Signup Page
    
    * Add Signup page and update routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 ﻿import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
 import AuthProvider from './context/AuthContext.jsx'
 
 import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import Members from './pages/Members.jsx'
 import AddMember from './pages/AddMember.jsx'
@@ -19,13 +20,17 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/" element={<ProtectedRoute><Layout><Dashboard/></Layout></ProtectedRoute>} />
+          <Route path="/dashboard" element={<ProtectedRoute><Layout><Dashboard/></Layout></ProtectedRoute>} />
           <Route path="/members" element={<ProtectedRoute><Layout><Members/></Layout></ProtectedRoute>} />
           <Route path="/add-member" element={<ProtectedRoute><Layout><AddMember/></Layout></ProtectedRoute>} />
           <Route path="/qr-generator" element={<ProtectedRoute><Layout><QRGenerator/></Layout></ProtectedRoute>} />
           <Route path="/scanner" element={<ProtectedRoute><Layout><Scanner/></Layout></ProtectedRoute>} />
           <Route path="/access-logs" element={<ProtectedRoute><Layout><AccessLogs/></Layout></ProtectedRoute>} />
           <Route path="/settings" element={<ProtectedRoute><Layout><Settings/></Layout></ProtectedRoute>} />
+          {/* Catch all route */}
+          <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
